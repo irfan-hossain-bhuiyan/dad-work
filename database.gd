25bed6473a95b08f8736c4f4e49d0964e6530864
irfan-hossain-bhuiyan <irfanhbhy@gmail.com>
2021-04-14 16:31:57 +0600

    this_one is started well

---
+++ database.gd
@@ -0,0 +1,14 @@
+extends Node2D
+class optics:
+	var optics_list=[]
+	var optics_name_list:PoolStringArray=[]
+	var Name:String
+	var Max_stock:int
+	var buy_price:int
+	var sold_price:int
+	var amount:=0
+	func create(name:String,max_stock:int,buy_price:int,sell_price:int):
+		
+			
+	
+	
