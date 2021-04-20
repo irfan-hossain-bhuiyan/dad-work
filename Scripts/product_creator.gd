bf74d6df17367a18db67cccdb941bc2ffb29b8b4
irfan-hossain-bhuiyan <irfanhbhy@gmail.com>
2021-04-20 17:36:35 +0600

    there is a bug in the signal

--- Scripts/product_creator.gd
+++ Scripts/product_creator.gd
@@ -6,10 +6,11 @@ func get_data():
 	var out=yield(self,"output")
 	return out
 func _on_project_data_box_confirmed() -> void:
+	print("it gone here")
 	emit_signal("output",$input_group.input_data())
 
 
 func _on_project_data_box_popup_hide() -> void:
-	emit_signal("output",0)
+	emit_signal("output",20)
 
 
--- Scripts/product_menu.gd
+++ Scripts/product_menu.gd
@@ -2,14 +2,17 @@ extends Control
 var button_label:PackedScene=preload("res://UI_item/product_UI/porduct_label.tscn")
 
 func _on_add_button_down() -> void:
-	var input=$project_data_box.get_data()
+	var input=yield($project_data_box.get_data(),"completed")
+	print_debug(input)
 	if input is Array:
 		var optic=Products.optics.new(input[0],input[2],input[1])
-		if optic.varify() is String:
-			pass
+		var error=optic.varify()
+		if error is String:
+			ErrorReport.error_message(error)
 		else:
 			optic.initiate()
 			$cover/head_line2/ScrollContainer/VBoxContainer.input_optics(optic)
+	
 
 
 			
